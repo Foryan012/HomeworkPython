e71429531989559c222d56d078e202cd5f5eb689
Foryan <Foryan2@yandex.ru>
2024-09-25 23:46:10 +1200

    .

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 x = int(input('Введите сумму вклада: '))
-p = int(input('Введите процентную ставку: '))
+p = float(input('Введите процентную ставку: '))
 y = int(input('Введите желаемую сумму: '))
+b = x + p + y
+print(round(b, 0))
 year = 0
 while x < y:
-    x = x + (x * p / 100)
+    x = x + ( x * p / 100)
     x = int(x)
     year += 1
 print('Через', year, 'лет сумма достигнет', y)
